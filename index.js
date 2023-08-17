55287a686c62b235111df2f2c7bcb35f65e2f58a
Kelvin Gil <gilkelvin2@gmail.com>
2023-08-17 18:30:18 -0400

    do again

--- index.js
+++ index.js
@@ -1,117 +1,117 @@
-findMatching = (driver,nameWeAreLooking4) => {
-  return driver.filter((name) => {
-    return name.toUpperCase() === nameWeAreLooking4.toUpperCase()
-  }) 
-}
-
-fuzzyMatch = (driver,nameWeAreLooking4) => {
-  return driver.filter((name) => {
-    return name[0,1] === nameWeAreLooking4[0,1]
-  });
-}
-
-matchName = (driver, nameWeAreLooking4) => {
-  return driver.filter((obj) => {
-    return obj.name === nameWeAreLooking4
-  });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function findMatching(driver,nameWeAreLookingFor){
+// findMatching = (driver,nameWeAreLooking4) => {
 //   return driver.filter((name) => {
-//     return name.toLowerCase() === nameWeAreLookingFor.toLowerCase(); 
-//   });
-
+//     return name.toUpperCase() === nameWeAreLooking4.toUpperCase()
+//   }) 
 // }
-// function fuzzyMatch(driver,names){
+
+// fuzzyMatch = (driver,nameWeAreLooking4) => {
 //   return driver.filter((name) => {
-//     return name[0] === names[0]; 
+//     return name[0,1] === nameWeAreLooking4[0,1]
 //   });
-
 // }
 
-// function matchName(driver,nameWeAreLookingFor){
-//   return driver.filter((object) => {
-//     return object.name === nameWeAreLookingFor; 
+// matchName = (driver, nameWeAreLooking4) => {
+//   return driver.filter((obj) => {
+//     return obj.name === nameWeAreLooking4
 //   });
-
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function findMatching(driver,nameWeAreLookingFor){
+  return driver.filter((name) => {
+    return name.toLowerCase() === nameWeAreLookingFor.toLowerCase(); 
+  });
+
+}
+function fuzzyMatch(driver,names){
+  return driver.filter((name) => {
+    return name[0] === names[0]; 
+  });
+
+}
+
+function matchName(driver,nameWeAreLookingFor){
+  return driver.filter((object) => {
+    return object.name === nameWeAreLookingFor; 
+  });
+
+}
